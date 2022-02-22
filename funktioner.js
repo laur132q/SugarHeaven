f5cdeff98a6043094d4d10513146a94a1d9e9986
Laura Kapper <laur132q@stud.kea.dk>
2022-02-22 23:14:19 +0100

    ny navigations bar

--- funktioner.js
+++ funktioner.js
@@ -1,61 +1,48 @@
 // ^ først definere jeg de forskellige varieable
-const btn = document.querySelector(".burger_menu");
+const btn = document.querySelector(".burger_menuSVG");
 const lukbtn = document.querySelector(".burger_menu_luk");
-const nav = document.querySelector(".nav_sidebar");
-const TopTekst = document.querySelector(".topInformation");
-const DeskNav = document.querySelector(".DeskNav");
-// const HeleNav = document.querySelector(".BurgerNav-DeskNav");
+const navbarLinks = document.querySelector(".navbar-links");
 
-// ^ så får jeg siden til at loade, altså når siden har loaded så skal den gå til funktionen
-// ^ "sidenVises"
-window.addEventListener("load", sidenVises);
-document.addEventListener("DOMContentLoaded", () => {
-  let resizer = new ResizeObserver(handleResize);
-  resizer.observe(document.querySelector(".BurgerNav-DeskNav"));
+btn.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+  lukbtn.classList.toggle("hidden");
+  btn.classList.toggle("hidden");
 });
 
+lukbtn.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+  lukbtn.classList.toggle("hidden");
+  btn.classList.toggle("hidden");
+});
+
+window.addEventListener("load", sidenVises);
+
 // ^I denne funktion gemmer den navigationen væk, og gemmer kryddset væk
-function sidenVises() {
-  nav.classList.add("hidden");
-  lukbtn.classList.add("hidden");
-}
+// function sidenVises() {
+//   // nav.classList.add("hidden");
+//   // lukbtn.classList.add("hidden");
+// }
 
 // ^ Her har jeg så lavet en funktion som hedder toggleMenu
 // ^ Når - den funktion aktiveres så toggler hidden på nav'en
 // ^ Den adder klassen hidden på min burger menu og fjerner hidden på krydset.
-function toggleMenu() {
-  console.log("toggleMenu");
+// function toggleMenu() {
+//   console.log("toggleMenu");
 
-  nav.classList.toggle("hidden");
+//   nav.classList.toggle("hidden");
 
-  btn.classList.toggle("hidden");
-  lukbtn.classList.toggle("hidden");
-}
+//   btn.classList.toggle("hidden");
+//   lukbtn.classList.toggle("hidden");
+// }
 
 // ^ Man kan sige at det er her jeg starter min funktion, fordi her får jeg den til at lytte efter et klik som så starter den
-btn.addEventListener("click", toggleMenu);
-lukbtn.addEventListener("click", toggleMenuLuk);
+// btn.addEventListener("click", toggleMenu);
+// lukbtn.addEventListener("click", toggleMenuLuk);
 
-function toggleMenuLuk() {
-  console.log("toggleMenu2");
-  nav.classList.toggle("hidden");
+// function toggleMenuLuk() {
+//   console.log("toggleMenu2");
+//   nav.classList.toggle("hidden");
 
-  btn.classList.toggle("hidden");
-  lukbtn.classList.toggle("hidden");
-}
-
-var x = window.matchMedia("(max-width:720px)");
-
-x.addListener(myFunction);
-myFunction(x);
-
-function myFunction(x) {
-  if (x.matches) {
-    btn.classList.toggle("hidden");
-    lukbtn.classList.toggle("hidden");
-    nav.classList.toggle("hidden");
-  } else {
-    TopTekst.classList.remove("hidden");
-    DeskNav.classList.remove("hidden");
-  }
-}
+//   btn.classList.toggle("hidden");
+//   lukbtn.classList.toggle("hidden");
+// }
